30a311b23cadf902ae57e13c8621097f5e7b8c91
tatiana <trodrigu7@cuc.edu.co>
2020-03-14 13:50:01 -0500

    14

---
+++ resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,17 @@
+def value(colors):
+    band_colors = {
+        "black": 0,
+        "brown": 1,
+        "red": 2,
+        "orange": 3,
+        "yellow": 4,
+        "green": 5,
+        "blue": 6,
+        "violet": 7,
+        "grey": 8,
+        "white": 9
+    }
+    code=""
+    for color in colors[:2]:
+        code += str(band_colors[color])
+    return int(code)
